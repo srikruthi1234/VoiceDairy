556806b8cf635b44db87755d40da3af4d4906972
kakeru14 <kakeru.otani@rakus-partners.co.jp>
2021-08-03 17:55:22 +0900

    heroku

--- routes/Routes.js
+++ routes/Routes.js
@@ -15,6 +15,12 @@ const cors = require("cors");
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 console.log('slsls');
 
 // get all todo items in the db
